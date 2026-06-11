b6bd296f1c1d040020051b4ebc506bb6310a0e81
Akshay Shetty <akshayshetty747@gmail.com>
2026-06-11 22:13:32 +0530

    Clean repository structure

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useAppContext } from './context/AppContext';
-import SetupPage from './components/layout/SetupPage';
-import Dashboard from './components/layout/Dashboard';
-
-export default function App() {
-  const { state } = useAppContext();
-  return state.agentReady ? <Dashboard /> : <SetupPage />;
-}
